9a6b3dbc9000755a1bab316ee9e837a6575b7f89
Sofiia Yamkova <sonaamkova530@gmail.com>
2026-05-18 14:37:01 +0300

    feat: configure image display in Django admin and fix Docker media volumes

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Product, Review, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'is_active', 'created_at')
-    list_filter = ('is_active',)
-    search_fields = ('name', 'description')
+    list_display = ['get_html_photo', 'name', 'price', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['name']
+
+    def get_html_photo(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="50" style="border-radius: 5 px;" />', obj.image.url)
+        return "Немає фото"
+    get_html_photo.short_description = "photo"
+
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
